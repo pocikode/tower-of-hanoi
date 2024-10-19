37184d3bf62cb0d0af2d2404ca89846290b45c8b
Agus Supriyatna <agus@motionpay.id>
2024-10-20 01:18:21 +0700

    apply game logic

--- src/game.ts
+++ src/game.ts
@@ -17,14 +17,35 @@ abstract class BaseElement {
 }
 
 class Rod extends BaseElement {
+  public size = 10;
   public color = "#2A2B5F";
+  public discs: Disc[] = [];
 
   public draw(ctx: CanvasRenderingContext2D) {
     ctx.fillStyle = this.color;
     ctx.beginPath();
-    ctx.roundRect(this.x, this.y, this.width, this.height, [10, 10, 0, 0]);
-    ctx.roundRect(this.x - 75, this.y + this.height, 160, this.width, 10);
+    ctx.roundRect(this.x, this.y + this.height, this.width, this.size, 10);
+    ctx.roundRect(
+      this.x + this.width / 2 - 5,
+      this.y + this.height,
+      this.size,
+      this.height - 280,
+      [0, 0, 10, 10],
+    );
     ctx.fill();
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+  }
+
+  public addDisc(disc: Disc) {
+    this.discs.push(disc);
+  }
+
+  public isAllowedDrop(disc: Disc): boolean {
+    return (
+      this.discs.length === 0 ||
+      this.discs[this.discs.length - 1].width > disc.width
+    );
   }
 }
 
@@ -47,6 +68,29 @@ class Disc extends BaseElement {
     ctx.roundRect(this.x, this.y, this.width, this.height, [7]);
     ctx.fill();
     ctx.stroke();
+  }
+
+  public isOnTop() {
+    return this.rod.discs[this.rod.discs.length - 1] === this;
+  }
+
+  public moveTo(rod: Rod) {
+    let i = 0;
+    for (i = 0; i < rod.discs.length; i++) {
+      if (rod.discs[i] === this) {
+        break;
+      }
+    }
+
+    if (this.rod !== rod) {
+      this.rod.discs = this.rod.discs.slice(0, this.rod.discs.length - 1);
+      this.rod = rod;
+      this.rod.addDisc(this);
+    }
+
+    this.x = rod.x + (rod.width - this.width) / 2;
+    this.y =
+      rod.y + rod.height - this.height - this.height * (rod.discs.length - 1);
   }
 }
 
@@ -80,14 +124,16 @@ class Button extends BaseElement {
 
 export class TowerOfHanoi {
   private ctx: CanvasRenderingContext2D;
+  private discTotal = 3;
+  private totalMoves = 0;
   private rods: Rod[] = [];
   private discs: Disc[] = [];
   private discColors: string[] = ["#FFEB55", "#EE66A6", "#D91656", "#640D5F"];
   private selectedDisc: Disc | null = null;
   private offsetX = 0;
   private offsetY = 0;
+  private isDone = false;
 
-  private discTotal = 4;
   private incrementBtn: Button;
   private decrementBtn: Button;
 
@@ -108,22 +154,30 @@ export class TowerOfHanoi {
   }
 
   private initGame() {
+    this.isDone = false;
+    this.totalMoves = 0;
     this.discs = [];
     this.rods = [
-      new Rod(100, this.canvas.height - 210, 10, 140),
-      new Rod(290, this.canvas.height - 210, 10, 140),
-      new Rod(480, this.canvas.height - 210, 10, 140),
+      new Rod(20, this.canvas.height - 200, 160, 140),
+      new Rod(210, this.canvas.height - 200, 160, 140),
+      new Rod(400, this.canvas.height - 200, 160, 140),
     ];
 
     for (let i = 0; i < this.discTotal; i++) {
       const height = 15;
       const width = 30 + 20 * (this.discTotal - i - 1);
-      const x = this.rods[0].x - this.rods[0].width * (this.discTotal - i);
+      const x =
+        this.rods[0].x +
+        this.rods[0].width / 2 -
+        this.rods[0].size * (this.discTotal - i) -
+        5;
       const y = this.rods[0].y + this.rods[0].height - height - height * i;
       const color = this.discColors[i % this.discColors.length];
 
       this.discs.push(new Disc(this.rods[0], x, y, width, 15, color));
     }
+
+    this.rods[0].discs = this.discs;
   }
 
   private draw() {
@@ -150,6 +204,10 @@ export class TowerOfHanoi {
       this.canvas.width - 155,
       this.canvas.height - 10,
     );
+
+    this.ctx.font = "bold 20px Arial";
+    this.ctx.fillStyle = "blue";
+    this.ctx.fillText(`Moves: ${this.totalMoves}`, 250, 32);
 
     this.incrementBtn.draw(this.ctx);
     this.decrementBtn.draw(this.ctx);
@@ -180,10 +238,12 @@ export class TowerOfHanoi {
     const mouseY = e.offsetY;
 
     const disc = this.discs.find((disc) => disc.isInside(mouseX, mouseY));
-    if (disc) {
-      this.selectedDisc = disc;
-      this.offsetX = mouseX - disc.x;
-      this.offsetY = mouseY - disc.y;
+    if (disc && !this.isDone) {
+      if (disc.isOnTop()) {
+        this.selectedDisc = disc;
+        this.offsetX = mouseX - disc.x;
+        this.offsetY = mouseY - disc.y;
+      }
     }
 
     if (this.incrementBtn.isInside(mouseX, mouseY)) {
@@ -196,14 +256,46 @@ export class TowerOfHanoi {
   }
 
   private onMouseMove(e: MouseEvent) {
+    const mouseX = e.offsetX;
+    const mouseY = e.offsetY;
+
     if (this.selectedDisc) {
-      this.selectedDisc.x = e.offsetX - this.offsetX;
-      this.selectedDisc.y = e.offsetY - this.offsetY;
+      this.selectedDisc.x = mouseX - this.offsetX;
+      this.selectedDisc.y = mouseY - this.offsetY;
       this.draw();
     }
   }
 
-  private onMouseUp() {
+  private onMouseUp(e: MouseEvent) {
+    const mouseX = e.offsetX;
+    const mouseY = e.offsetY;
+
+    if (this.selectedDisc) {
+      this.selectedDisc.x = mouseX - this.offsetX;
+      this.selectedDisc.y = mouseY - this.offsetY;
+
+      const rod = this.rods.find(
+        (rod) => rod !== this.selectedDisc?.rod && rod.isInside(mouseX, mouseY),
+      );
+      if (rod) {
+        if (rod.isAllowedDrop(this.selectedDisc)) {
+          this.totalMoves++;
+          this.selectedDisc.moveTo(rod);
+          this.isDone = this.rods[2].discs.length === this.discTotal;
+        } else {
+          this.selectedDisc.moveTo(this.selectedDisc.rod);
+        }
+      } else {
+        this.selectedDisc.moveTo(this.selectedDisc.rod);
+      }
+
+      this.draw();
+    }
+
+    if (this.isDone) {
+      this.drawCongratulation();
+    }
+
     this.selectedDisc = null;
   }
 
@@ -221,5 +313,11 @@ export class TowerOfHanoi {
       this.initGame();
       this.draw();
     }
+  }
+
+  private drawCongratulation() {
+    this.ctx.font = "700 35px Arial";
+    this.ctx.fillStyle = "#FFEB55";
+    this.ctx.fillText("Well Done!", 200, 80);
   }
 }
