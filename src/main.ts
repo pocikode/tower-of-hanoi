76701056e52c4e6424c7ae4c459857954491d7ba
Agus Supriyatna <agus@motionpay.id>
2024-10-23 22:11:51 +0700

    add github link

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { TowerOfHanoi } from "./game";
+import githubIcon from "./icon/github.svg";
 
 window.addEventListener("load", () => {
-	const canvas = document.getElementById("game") as HTMLCanvasElement;
-	new TowerOfHanoi(canvas);
+  const canvas = document.getElementById("game") as HTMLCanvasElement;
+  new TowerOfHanoi(canvas);
+
+  const githubLink = document.querySelector("#github-link") as HTMLDivElement;
+  githubLink.innerHTML = `
+    <a href="https://github.com/pocikode/tower-of-hanoi" target="_blank">
+      <img src="${githubIcon}" alt="Github Logo" class="github-icon w-10" />
+    </a>
+  `;
 });
